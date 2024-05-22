8950a8db86a0d32ec85964c5e72d8e0df8b3838e
AndresZapata <andreszapataqr@gmail.com>
2024-05-22 13:11:35 -0500

    update personalizacions table

--- database/migrations/2024_05_09_212710_create_personalizacions_table.php
+++ database/migrations/2024_05_09_212710_create_personalizacions_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreatePersonalizacionesTable extends Migration
+class CreatePersonalizacionsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class CreatePersonalizacionesTable extends Migration
      */
     public function up()
     {
-        Schema::create('personalizaciones', function (Blueprint $table) {
+        Schema::create('personalizacions', function (Blueprint $table) {
             $table->engine = "InnoDB";
 
             $table->id();
