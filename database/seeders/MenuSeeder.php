710be4cfc9832cce01fab2252e84afc5b6ba25cc
Guillermo Gomez <guigofu@gmail.com>
2024-05-31 16:15:24 -0500

    Added custom sale to menu seeder

--- database/seeders/MenuSeeder.php
+++ database/seeders/MenuSeeder.php
@@ -18,42 +18,49 @@ class MenuSeeder extends Seeder
         $inicio = new Menu();
         $inicio->title = "Inicio";
         $inicio->route = "index";
-        $inicio->state_id = 1; 
+        $inicio->state_id = 1;
         $inicio->menu_type_id = 2;
         $inicio->save();
 
         $murales = new Menu();
         $murales->title = "Murales";
         $murales->route = "galleries.index";
-        $murales->state_id = 1; 
+        $murales->state_id = 1;
         $murales->menu_type_id = 1;
         $murales->save();
-        
+
         $rutaCultural = new Menu();
         $rutaCultural->title = "Ruta cultural";
         $rutaCultural->route = "culturalRoute";
-        $rutaCultural->state_id = 1; 
+        $rutaCultural->state_id = 1;
         $rutaCultural->menu_type_id = 1;
         $rutaCultural->save();
-        
+
         $tienda = new Menu();
         $tienda->title = "Tienda";
         $tienda->route = "products.index";
-        $tienda->state_id = 1; 
+        $tienda->state_id = 1;
         $tienda->menu_type_id = 1;
         $tienda->save();
-        
+
+        $customSale = new Menu();
+        $customSale->title = "Venta personalizada";
+        $customSale->route = "personalizedSaleCreate";
+        $customSale->state_id = 1;
+        $customSale->menu_type_id = 1;
+        $customSale->save();
+
         $iniciarSesion = new Menu();
         $iniciarSesion->title = "Descargar APP";
         $iniciarSesion->route = "login";
-        $iniciarSesion->state_id = 1; 
+        $iniciarSesion->state_id = 1;
         $iniciarSesion->menu_type_id = 1;
         $iniciarSesion->save();
-        
+
         $carrito = new Menu();
         $carrito->title = "Carrito";
         $carrito->route = "index";
-        $carrito->state_id = 1; 
+        $carrito->state_id = 1;
         $carrito->menu_type_id = 1;
         $carrito->save();
     }
